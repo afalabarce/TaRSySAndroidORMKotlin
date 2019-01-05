f4231c69e21fdd7f6d90fa9b4b26e6a804ade878
Antonio Fdez. Alabarce <tarsysdev@gmail.com>
2019-01-05 22:11:16 +0100

    Delete Indexes.kt

--- kotlin-orm/src/main/java/com/github/tarsys/android/kotlin/orm/annotations/Indexes.kt
+++
@@ -1,7 +0,0 @@
-package com.github.tarsys.android.kotlin.orm.annotations
-
-import com.github.tarsys.android.kotlin.orm.enums.OrderCriteria
-import kotlin.annotation.Retention
-@Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class Indexes(val value: Array<Index>)
