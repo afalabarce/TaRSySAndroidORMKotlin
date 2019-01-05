948182697e1d8515811f16e4b23bc2b37a61c851
Antonio Fdez. Alabarce <tarsysdev@gmail.com>
2019-01-05 22:10:25 +0100

    Delete Filter.kt

--- kotlin-orm/src/main/java/com/github/tarsys/android/kotlin/orm/engine/Filter.kt
+++
@@ -1,8 +0,0 @@
-package com.github.tarsys.android.kotlin.orm.engine
-
-import com.github.tarsys.android.support.utilities.AndroidSupport
-
-class Filter {
-    var FilterString: String = AndroidSupport.EmptyString
-    val FilterData: ArrayList<String> = arrayListOf()
-}
