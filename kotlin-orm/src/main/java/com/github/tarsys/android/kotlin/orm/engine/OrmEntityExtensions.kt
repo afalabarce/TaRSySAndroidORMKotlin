ccb212460f98907e0e8602a578b6fd69a5556cc3
tarsys <tarsysdev@gmail.com>
2019-01-06 22:00:27 +0100

    Source code Refactor

--- kotlin-orm/src/main/java/com/github/tarsys/android/kotlin/orm/engine/OrmEntityExtensions.kt
+++ kotlin-orm/src/main/java/com/github/tarsys/android/kotlin/orm/engine/OrmEntityExtensions.kt
@@ -1,33 +1,130 @@
 package com.github.tarsys.android.kotlin.orm.engine
 
 import android.content.ContentValues
-import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.util.Log
 import com.github.tarsys.android.kotlin.orm.annotations.TableField
 import com.github.tarsys.android.kotlin.orm.dataobjects.DataTable
+import com.github.tarsys.android.kotlin.orm.engine.utilities.toFilter
+import com.github.tarsys.android.kotlin.orm.engine.utilities.toInt
 import com.github.tarsys.android.kotlin.orm.enums.DBDataType
 import com.github.tarsys.android.kotlin.orm.interfaces.IOrmEntity
 import com.github.tarsys.android.kotlin.orm.sqlite.SQLiteSupport
 import com.github.tarsys.android.support.utilities.AndroidSupport
-import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.reflect.KClass
 import kotlin.reflect.KMutableProperty
 import kotlin.reflect.KProperty
 import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.isSubtypeOf
 import kotlin.reflect.full.starProjectedType
 
+//region Useful query methods
+
+fun SQLiteDatabase.rawQueryToDataTable(sqlQuery: String): DataTable? = this.rawQueryToDataTable(sqlQuery, false)
+
+fun SQLiteDatabase.rawQueryToDataTable(sqlQuery: String, forceOnlyDate: Boolean): DataTable?{
+    try{
+        val cursor = this.rawQuery(sqlQuery, null)
+        if (cursor != null){
+            val returnValue = DataTable()
+            cursor.moveToFirst()
+            cursor.columnNames.forEach { column ->
+                // TODO: Pending implementing datasource read...
+            }
+        }
+    }catch (ex: Exception){
+        Log.e("rawQueryToDataTable", ex.message)
+        ex.printStackTrace()
+    }
+
+    return null
+}
+
+fun <T:IOrmEntity> KClass<T>.filterObjectQuery(filterMap: Map<String, Any>): ArrayList<T>{
+    val returnValue: ArrayList<T> = arrayListOf()
+    // TODO: Pending implementing object filter read...
+
+    return returnValue
+}
+
+//endregion
+
+//region CRUD methods
+
+/**
+ * Gets entity identified by its primary key
+ * @return true if the entity has been loaded from database, false otherwise
+ */
+fun <T:IOrmEntity>T.read(): Boolean = this.read(true)
+
+/**
+ * Gets entity identified by its primary key
+ * @param recursiveLoad true if the entity needs to be fully loaded, false otherwise
+ * @return true if the entity has been loaded from database, false otherwise
+ */
+fun <T:IOrmEntity>T.read(recursiveLoad: Boolean): Boolean{
+    val sqliteDb = SGBDEngine.SQLiteDataBase(true)
+
+    if (sqliteDb != null){
+        val returnValue = this.findEntityByPrimaryKey(sqliteDb, recursiveLoad)
+        sqliteDb.close()
+
+        return returnValue
+    }
+
+    return false
+}
+
+/**
+ * delete entity identified by its primary key
+ * @return true if the entity has been deleted from database, else otherwise
+ */
+fun <T: IOrmEntity>T.delete(): Boolean{
+    var returnValue: Boolean = false
+    val sqliteDb = SGBDEngine.SQLiteDataBase(false)
+
+    if (sqliteDb?.isOpen == true){
+        val dbEntity = this.javaClass.kotlin.dbEntity
+        if (dbEntity != null){
+            val pkFilter = this.primaryKeyFilter(false)
+            if (pkFilter != null){
+
+                sqliteDb.beginTransaction()
+
+                try{
+                    returnValue = this.delete(sqliteDb)
+
+                    if (returnValue) sqliteDb.setTransactionSuccessful()
+
+                }catch (ex: java.lang.Exception){
+
+                }finally {
+                    sqliteDb.endTransaction()
+                }
+            }
+        }
+    }
+
+    return returnValue
+}
+
+/**
+ * persist entity into database (needs the entity fully loaded)
+ * @return true if the entity has been persisted into database, false otherwise
+ */
+fun <T: IOrmEntity>T.save(): Boolean = this.save(true)
+
+//endregion
+
 //region Useful query properties
 
-fun Boolean.toInt() = if (this) 1 else 0
-
-val <T: IOrmEntity>T.hasFieldEntities: Boolean
+private val <T: IOrmEntity>T.hasFieldEntities: Boolean
     get() = !this.javaClass.kotlin.entityFields.isEmpty() || !this.javaClass.kotlin.entityListFields.isEmpty()
 
-fun <T: IOrmEntity>T.entityReport(includeWithoutCascadeDelete: Boolean): ArrayList<IOrmEntity>{
+private fun <T: IOrmEntity>T.entityReport(includeWithoutCascadeDelete: Boolean): ArrayList<IOrmEntity>{
     val returnValue: ArrayList<IOrmEntity> = arrayListOf()
 
     if (this.hasFieldEntities){
@@ -51,7 +148,7 @@ fun <T: IOrmEntity>T.entityReport(includeWithoutCascadeDelete: Boolean): ArrayLi
     return returnValue
 }
 
-fun <T: IOrmEntity>T.primaryKeyFilter(forTableRelation: Boolean): Filter?
+private fun <T: IOrmEntity>T.primaryKeyFilter(forTableRelation: Boolean): Filter?
 {
     try{
         val dbEntity = this.javaClass.kotlin.dbEntity
@@ -102,178 +199,44 @@ fun <T: IOrmEntity>T.primaryKeyFilter(forTableRelation: Boolean): Filter?
     return null
 }
 
-val <T: IOrmEntity>T.foreignKeyContentValues: ContentValues?
-get(){
-    var returnValue: ContentValues? = null
-
-    try{
-        val dbEntity = this.javaClass.kotlin.dbEntity
-
-        if (dbEntity != null){
-            returnValue = ContentValues(0)
-            val primaryKeyProperties = this.javaClass.kotlin.primaryKeyProperties
-
-            for (field in primaryKeyProperties){
-                val fieldName = field.foreignKeyFieldName(dbEntity)
-                val fieldValue = field.getter.call(this)
-
-                when(field.tableField!!.DataType){
-                    DBDataType.StringDataType, DBDataType.TextDataType -> returnValue.put(fieldName, fieldValue as? String ?: field.tableField!!.DefaultValue)
-                    DBDataType.IntegerDataType, DBDataType.LongDataType -> returnValue.put(fieldName, fieldValue?.toString() ?: field.tableField!!.DefaultValue)
-                    DBDataType.RealDataType -> returnValue.put(fieldName, fieldValue?.toString()?.replace(",",".") ?: field.tableField!!.DefaultValue)
-                    DBDataType.DateDataType -> returnValue.put(fieldName, (fieldValue as? Date)?.time?.toString() ?: field.tableField!!.DefaultValue ?: "0")
-                }
-            }
-        }
-    }catch (ex: java.lang.Exception){
-        returnValue = null
-        Log.e("foreignKeyContentValues", ex.message)
-        ex.printStackTrace()
-    }
-
-    return returnValue
-}
-
-val ContentValues.toFilter: Filter?
+private val <T: IOrmEntity>T.foreignKeyContentValues: ContentValues?
     get(){
-        try{
-            if (this.size() > 0){
-                val returnValue = Filter()
-                for (entry in this.valueSet()){
-                    returnValue.FilterString += if (returnValue.FilterString.trim().equals(AndroidSupport.EmptyString)) AndroidSupport.EmptyString else " and "
-                    returnValue.FilterString += "${entry.key}=?"
-                    returnValue.FilterData.add(entry.value.toString())
-                }
+        var returnValue: ContentValues? = null
 
-                return returnValue
+        try{
+            val dbEntity = this.javaClass.kotlin.dbEntity
+
+            if (dbEntity != null){
+                returnValue = ContentValues(0)
+                val primaryKeyProperties = this.javaClass.kotlin.primaryKeyProperties
+
+                for (field in primaryKeyProperties){
+                    val fieldName = field.foreignKeyFieldName(dbEntity)
+                    val fieldValue = field.getter.call(this)
+
+                    when(field.tableField!!.DataType){
+                        DBDataType.StringDataType, DBDataType.TextDataType -> returnValue.put(fieldName, fieldValue as? String ?: field.tableField!!.DefaultValue)
+                        DBDataType.IntegerDataType, DBDataType.LongDataType -> returnValue.put(fieldName, fieldValue?.toString() ?: field.tableField!!.DefaultValue)
+                        DBDataType.RealDataType -> returnValue.put(fieldName, fieldValue?.toString()?.replace(",",".") ?: field.tableField!!.DefaultValue)
+                        DBDataType.DateDataType -> returnValue.put(fieldName, (fieldValue as? Date)?.time?.toString() ?: field.tableField!!.DefaultValue ?: "0")
+                    }
+                }
             }
-        }catch (ex:java.lang.Exception){
-            Log.e("ContentValues.toFilter", ex.message)
+        }catch (ex: java.lang.Exception){
+            returnValue = null
+            Log.e("foreignKeyContentValues", ex.message)
             ex.printStackTrace()
         }
 
-        return null
+        return returnValue
     }
 
 //endregion
 
-//region Useful query methods
 
-inline fun <reified T: IOrmEntity>Filter.execute(sqliteDb: SQLiteDatabase): ArrayList<T>{
-    val returnValue: ArrayList<T> = arrayListOf()
-    try{
-        val dbEntity = T::class.dbEntity
-        val returnType = T::class
+//region undocumented private support crud methods
 
-        if (dbEntity != null) {
-            val navQuery = sqliteDb.query(
-                dbEntity.TableName,
-                null,
-                this.FilterString,
-                this.FilterData.toTypedArray(),
-                null,
-                null,
-                null
-            )
-
-            while (navQuery?.moveToNext() == true) {
-                val entity = (returnType.createFilteringEntity(returnType, navQuery) as T)
-                val obj = entity.findEntityByPrimaryKey(sqliteDb, true)
-                if (obj)
-                    returnValue += entity
-            }
-        }
-    }catch (ex:Exception){
-        Log.e("IOrmEntity.filter", ex.message)
-        ex.printStackTrace()
-    }
-
-    return returnValue
-}
-
-inline fun <reified T:IOrmEntity>SQLiteDatabase.filter(whereFilter: String, whereValues: Array<String>, orderBy: String, recursiveLoad: Boolean): ArrayList<T>{
-    val returnValue: ArrayList<T> = arrayListOf()
-    val dbEntity = T::class.dbEntity
-    try{
-        if (dbEntity != null && this.isOpen){
-            val query = this.query(dbEntity.TableName, null, whereFilter, whereValues, null,null, orderBy)
-
-            while (query?.moveToNext() == true){
-                val pkEntity = T::class.createFilteringEntity(T::class, query) as? T
-                if (pkEntity != null){
-                    val entity = pkEntity.findEntityByPrimaryKey(this, true)
-                    if (entity) returnValue += pkEntity
-                }
-            }
-        }
-    }catch (ex: Exception){
-        returnValue.clear()
-        Log.e("SQLiteDatabase.filter", ex.message)
-        ex.printStackTrace()
-    }
-
-
-    return returnValue
-}
-
-fun SQLiteDatabase.rawQueryToDataTable(sqlQuery: String): DataTable? = this.rawQueryToDataSource(sqlQuery, false)
-
-fun SQLiteDatabase.rawQueryToDataSource(sqlQuery: String, forceOnlyDate: Boolean): DataTable?{
-    try{
-        val cursor = this.rawQuery(sqlQuery, null)
-        if (cursor != null){
-            val returnValue = DataTable()
-            cursor.moveToFirst()
-            cursor.columnNames.forEach { column ->
-                // TODO: Pending implementing datasource read...
-            }
-        }
-    }catch (ex: Exception){
-        Log.e("rawQueryToDataSource", ex.message)
-        ex.printStackTrace()
-    }
-
-    return null
-}
-
-fun <T:IOrmEntity>SQLiteDatabase.filterObjectQuery(): ArrayList<T>{
-    val returnValue: ArrayList<T> = arrayListOf()
-    // TODO: Pending implementing object filter read...
-
-    return returnValue
-}
-
-//endregion
-
-//region CRUD methods
-
-fun <T:IOrmEntity>T.read(): Boolean{
-    val sqliteDb = SGBDEngine.SQLiteDataBase(true)
-
-    if (sqliteDb != null){
-        val returnValue = this.findEntityByPrimaryKey(sqliteDb, true)
-        sqliteDb.close()
-
-        return returnValue
-    }
-
-    return false
-}
-
-fun <T:IOrmEntity>T.read(recursiveLoad: Boolean): Boolean{
-    val sqliteDb = SGBDEngine.SQLiteDataBase(true)
-
-    if (sqliteDb != null){
-        val returnValue = this.findEntityByPrimaryKey(sqliteDb, recursiveLoad)
-        sqliteDb.close()
-
-        return returnValue
-    }
-
-    return false
-}
-
-fun <T: IOrmEntity>T.findEntityByPrimaryKey(sqliteDb: SQLiteDatabase?, recursiveLoad: Boolean): Boolean {
+private fun <T: IOrmEntity>T.findEntityByPrimaryKey(sqliteDb: SQLiteDatabase?, recursiveLoad: Boolean): Boolean {
     try{
         val dbEntity = this::class.dbEntity
 
@@ -383,35 +346,6 @@ fun <T: IOrmEntity>T.findEntityByPrimaryKey(sqliteDb: SQLiteDatabase?, recursive
     return false
 }
 
-fun <T: IOrmEntity>T.delete(): Boolean{
-    var returnValue: Boolean = false
-    val sqliteDb = SGBDEngine.SQLiteDataBase(false)
-
-    if (sqliteDb?.isOpen == true){
-        val dbEntity = this.javaClass.kotlin.dbEntity
-        if (dbEntity != null){
-            val pkFilter = this.primaryKeyFilter(false)
-            if (pkFilter != null){
-
-                sqliteDb.beginTransaction()
-
-                try{
-                    returnValue = this.delete(sqliteDb)
-
-                    if (returnValue) sqliteDb.setTransactionSuccessful()
-
-                }catch (ex: java.lang.Exception){
-
-                }finally {
-                    sqliteDb.endTransaction()
-                }
-            }
-        }
-    }
-
-    return returnValue
-}
-
 private fun <T: IOrmEntity>T.delete(sqliteDb: SQLiteDatabase): Boolean{
     try{
         val dbEntity = this.javaClass.kotlin.dbEntity
@@ -433,9 +367,7 @@ private fun <T: IOrmEntity>T.delete(sqliteDb: SQLiteDatabase): Boolean{
                 }
                 if (results.firstOrNull { x -> !x } != true ){
                     // and finally, this entity...
-                    var returnValue = sqliteDb.delete(dbEntity.TableName, pkFilter.FilterString, pkFilter.FilterData.toTypedArray()) > 0
-
-                    return returnValue
+                    return sqliteDb.delete(dbEntity.TableName, pkFilter.FilterString, pkFilter.FilterData.toTypedArray()) > 0
                 }
             }
         }
@@ -447,9 +379,7 @@ private fun <T: IOrmEntity>T.delete(sqliteDb: SQLiteDatabase): Boolean{
     return false
 }
 
-fun <T: IOrmEntity>T.save(): Boolean = this.save(true)
-
-fun <T: IOrmEntity>T.save(forceEntitySave: Boolean): Boolean{
+private fun <T: IOrmEntity>T.save(forceEntitySave: Boolean): Boolean{
     val sqliteDb = SGBDEngine.SQLiteDataBase(false)
 
     if (sqliteDb?.isOpen == true){
@@ -467,7 +397,7 @@ fun <T: IOrmEntity>T.save(forceEntitySave: Boolean): Boolean{
     return false
 }
 
-fun <T: IOrmEntity>T.save(sqliteDb: SQLiteDatabase, forceEntitySave: Boolean): Boolean{
+private fun <T: IOrmEntity>T.save(sqliteDb: SQLiteDatabase, forceEntitySave: Boolean): Boolean{
     try{
         val dbEntity = this.javaClass.kotlin.dbEntity
 
@@ -479,8 +409,8 @@ fun <T: IOrmEntity>T.save(sqliteDb: SQLiteDatabase, forceEntitySave: Boolean): B
                 val entityListFields: ArrayList<IOrmEntity> = arrayListOf()
                 val entityListTableFields: ArrayList<TableField> = arrayListOf()
 
-                var error: Boolean = false
-                var autoIncrementField: KProperty<*>? = this.javaClass.kotlin.tableFieldProperties.firstOrNull { x -> x.tableField!!.AutoIncrement }
+                var error = false
+                val autoIncrementField: KProperty<*>? = this.javaClass.kotlin.tableFieldProperties.firstOrNull { x -> x.tableField!!.AutoIncrement }
 
                 val fields = this.javaClass.kotlin.tableFieldProperties.filter { x -> !x.tableField!!.AutoIncrement }
 
@@ -493,7 +423,7 @@ fun <T: IOrmEntity>T.save(sqliteDb: SQLiteDatabase, forceEntitySave: Boolean): B
                         when (field.dbDataType){
                             DBDataType.StringDataType, DBDataType.TextDataType -> contentValues.put(field.fieldName, fieldValue as? String ?: field.tableField!!.DefaultValue)
                             DBDataType.Serializable -> contentValues.put(field.fieldName, GsonBuilder().create().toJson(fieldValue))
-                            DBDataType.RealDataType -> contentValues.put(field.fieldName, fieldValue as? Float ?: field.tableField!!.DefaultValue?.toFloat() ?: 0f)
+                            DBDataType.RealDataType -> contentValues.put(field.fieldName, fieldValue as? Float ?: field.tableField!!.DefaultValue.toFloat() ?: 0f)
                             DBDataType.IntegerDataType, DBDataType.LongDataType -> contentValues.put(field.fieldName, fieldValue as? Long ?: (fieldValue as? Int)?.toLong() ?: try{ field.tableField!!.DefaultValue?.toLong()}catch (ex:Exception){ 0L } ?: 0)
                             DBDataType.BooleanDataType -> contentValues.put(field.fieldName, if (fieldValue as? Boolean ?: field.tableField!!.DefaultValue == "1") 1 else 0)
                             DBDataType.DateDataType -> contentValues.put(field.fieldName, (fieldValue as? Date)?.time ?: 0 )
@@ -502,8 +432,13 @@ fun <T: IOrmEntity>T.save(sqliteDb: SQLiteDatabase, forceEntitySave: Boolean): B
                                 contentValues.putAll((fieldValue as? IOrmEntity)?.foreignKeyContentValues)
                             }
                             DBDataType.EntityListDataType -> {
-                                entityListFields += fieldValue as ArrayList<IOrmEntity>
-                                entityListTableFields += field.tableField!!
+                                try {
+                                    entityListFields += fieldValue as ArrayList<IOrmEntity>
+                                    entityListTableFields += field.tableField!!
+                                }catch (ex: Exception){
+                                    Log.e("IOrmEntity.save::EList", ex.message)
+                                    ex.printStackTrace()
+                                }
                             }
                             DBDataType.EnumDataType -> {
                                 if (field.returnType::class.java.isEnum){
@@ -579,6 +514,7 @@ fun <T: IOrmEntity>T.save(sqliteDb: SQLiteDatabase, forceEntitySave: Boolean): B
                             if (!fgFilter.isEmpty() && !fgValues.isEmpty()) sqliteDb.delete(entityTableName, fgFilter, fgValues.toTypedArray())
                         }
 
+                        queryEntity?.close()
                         sqliteDb.delete(relatedTable, filterRelations.FilterString, filterRelations.FilterData.toTypedArray())
                     }
 
@@ -601,8 +537,7 @@ fun <T: IOrmEntity>T.save(sqliteDb: SQLiteDatabase, forceEntitySave: Boolean): B
                             }
 
                             try{
-                                val entityId = sqliteDb.insert(relatedTable, null, cValues)
-
+                                sqliteDb.insert(relatedTable, null, cValues)
                             }catch (ex:Exception){
                                 Log.e("IOrmEntity.save:Y", ex.message)
                                 ex.printStackTrace()
@@ -621,5 +556,6 @@ fun <T: IOrmEntity>T.save(sqliteDb: SQLiteDatabase, forceEntitySave: Boolean): B
 
     return false
 }
+
 
 //endregion
